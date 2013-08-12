d31480b5804d50c5041ba4a5843d0549b020f647
David Adams <dmadams28@gmail.com>
2013-08-12 15:38:47 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Rails2Basic::Application.config.secret_key_base = 'ed628c953ec2b43234d3dc32d14a9916ef630e8749ad7a864f138f7588e7f9f0e10ccb0b7331260c78cf2f9e8192bdf11b3c95add5eb9891c4c6e100f4bd2dac'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Rails2Basic::Application.config.session_store :cookie_store, key: '_rails2_basic_session'
